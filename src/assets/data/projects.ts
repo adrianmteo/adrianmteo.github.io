e9a12760dd0cdcabeb8c2e4e135e7b8494be6203
Adrian Mateoaea <adrianmteo@gmail.com>
2024-01-29 20:34:29 +0200

    Add projects

---
+++ src/assets/data/projects.ts
@@ -0,0 +1,17 @@
+export const projects = [
+  {
+    name: "Simple Debt Tracker",
+    platform: "iOS",
+    description:
+      "Easily track debts from various people, events or categories with an intuitive and easy design.",
+    src: "/images/simple-debt-tracker.png",
+    href: "https://apps.apple.com/ro/app/simple-debt-tracker/id6476518584",
+  },
+  {
+    name: "Luna",
+    platform: "Windows",
+    description: "A super lightweight automatic theme changer for Windows 10.",
+    src: "/images/luna.png",
+    href: "https://github.com/adrianmteo/Luna",
+  },
+];
