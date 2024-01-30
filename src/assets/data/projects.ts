dca8190aa33833661996df31d3824080cd2c98da
Adrian Mateoaea <adrianmteo@gmail.com>
2024-01-30 14:54:23 +0200

    Update url

--- src/assets/data/projects.ts
+++ src/assets/data/projects.ts
@@ -5,7 +5,7 @@ export const projects = [
     description:
       "Easily track debts from various people, events or categories with an intuitive and easy design.",
     src: "/images/simple-debt-tracker.png",
-    href: "https://apps.apple.com/ro/app/simple-debt-tracker/id6476518584",
+    href: "https://apps.apple.com/app/simple-debt-tracker/id6476518584",
   },
   {
     name: "Luna",
