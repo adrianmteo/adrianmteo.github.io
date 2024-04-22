3ddcf2e91fd02a8aab67beb3a1a2bc9a0567abd3
Adrian Mateoaea <adrianmteo@gmail.com>
2024-04-22 15:41:58 +0300

    Tweaks

--- src/assets/data/projects.ts
+++ src/assets/data/projects.ts
@@ -1,5 +1,12 @@
 export const projects = [
   {
+    name: "Simple Anything Counter",
+    platform: "iOS",
+    description: "Count anything with an intuitive and easy design.",
+    src: "/images/simple-anything-counter.png",
+    href: "https://apps.apple.com/app/simple-anything-counter/id6499043013",
+  },
+  {
     name: "Simple Debt Tracker",
     platform: "iOS",
     description:
